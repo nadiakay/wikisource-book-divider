e7c99afed5849a96aaa5fbe10aae22a789487912
Nadia <nadiakrakowski@gmail.com>
2022-05-28 03:40:28 -0500

    break up different functions. todo: fix windows compatibility issue

--- divideBook.py
+++ divideBook.py
@@ -13,43 +13,60 @@ import zipfile
 import json
 from bs4 import BeautifulSoup
 
-
-def parse_metadata(path):
-    with open(path) as fp:
-        soup = BeautifulSoup(fp, "xml")
-    title = soup.find('dc:title').contents[0]
-    authors = [author.contents[0] for author in soup.find_all('dc:creator')]
-    return title, authors
-
-
+"""
+unzip_book: takes <book>.htmlz as argument. returns path of book directory
+"""
 def unzip_book(path):
     dir = os.path.split(path)[0] + os.path.split(path)[1].split('.')[0]
     with zipfile.ZipFile(path, 'r') as zip_ref:
         zip_ref.extractall(dir)
     return dir
 
+"""
+strip_tags: takes html fragment as argument. returns detagged soup object.
+"""
+def strip_tags(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    for tag in soup.find_all(True):
+        tag.replaceWithChildren()
+    return soup
 
-def divide_book(path):
-    with open(path) as fp:
+"""
+cleanup_book: removes extraneous tags from book and renames internal links to
+fit next.js project directory.
+takes <book_dir>/index.html as argument.
+"""
+def cleanup_book(fn):
+    with open(fn, 'r', encoding='utf-8') as fp:
         soup = BeautifulSoup(fp, 'html.parser')
 
-    [tag.parent.decompose() for tag in soup.find_all(
-        "span", {'data-page-index': True})]
+    [tag.parent.decompose() for tag in soup.find_all("span", {'data-page-index': True})]
 
-    # todo: move this to a different script
     for img in soup.find_all("img"):
         img["src"] = os.path.join('/assets/book/',
-                                  os.path.split(path)[0], os.path.split(img["src"])[1])
+                                  os.path.split(fn)[0], os.path.split(img["src"])[1])
+
+
+    return 0
+
+"""
+split_book: splits html book from ws-export into files by chapter.
+takes <book_dir>/index.html as argument. returns json representation of
+chapters.
+"""
+def split_book(fn):
+    with open(fn, 'r', encoding='utf-8') as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
 
     divs = soup.find_all('div', {'class': 'mw-content-ltr'})
     toc_links = divs[0].find_all('a', href=re.compile("#calibre_link"))
-    dir = os.path.split(path)[0]
-    chapters = []
-
+    dir = os.path.split(fn)[0]
     try:
         os.mkdir(dir)
     except FileExistsError:
         pass
+    
+    chapters = []
 
     for i, div in reversed(list(enumerate(divs, start=0))):
         if i == len(divs) - 1:
@@ -58,32 +75,55 @@ def divide_book(path):
         title = None
         for toc_link in toc_links:
             if toc_link["href"] == "#" + div["id"]:
-                title = toc_link.contents[0]
+            # check if the chapter's parent div matches a table of contents link, and if so get title and rename the toc link
+                title = strip_tags(str(toc_link.contents[0]))
                 divs[0].find('a', {'href': toc_link["href"]})[
-                    "href"] = "./" + str(i) + ".html"
+                    "href"] = "./" + str(i)
             else:
                 link = div.find(id=toc_link["href"][1:])
                 if link != None:
+                # and also check other el's for toc links to subsections
                     divs[0].find('a', {'href': toc_link["href"]})[
-                        "href"] = "./" + str(i) + ".html" + "#" + link["id"]
+                        "href"] = "./" + str(i) + "#" + link["id"]
 
         filename = dir.split('/')[-1] + "/" + str(i) + ".html"
-        chapters.append({"title": title, "id": i})
-        with open(filename, "w") as file:
+        chapters.append({"title": str(title), "id": i})
+        with open(filename, "w", encoding="utf-8") as file:
             file.write(str(div))
     return chapters
 
+"""
+parse_metadata: takes book directory as arg. returns title and authors 
+"""
+def parse_metadata(path):
+    with open(path + '/metadata.opf') as fp:
+        soup = BeautifulSoup(fp, "xml")
+    title = soup.find('dc:title').contents[0]
+    authors = [author.contents[0] for author in soup.find_all('dc:creator')]
+    return title, authors
+
+"""
+write_json: writes 
+takes book directory and json chapters returned from split_book as arguments.
+returns json data object.
+"""
+def write_json(dir, chapters):
+    title, authors = parse_metadata(dir)
+    data = {"title": str(title), "authors": authors,
+            "slug": dir, "chapters": chapters}
+    print("data:", data)
+    with open(dir + '/data.json', 'w') as f:
+        json.dump(data, f)
+    return data
 
 if __name__ == '__main__':
     books = sys.argv[1:]
     for book in books:
         dir = unzip_book(book)
-        chapters = list(reversed(divide_book(dir + '/index.html')))
-        title, authors = parse_metadata(dir + '/metadata.opf')
-        data = {"title": title, "authors": authors,
-                "slug": dir, "chapters": chapters}
+        chapters = list(reversed(split_book(dir + '/index.html')))
+        data = write_json(dir, chapters)
+        print("data:", data)
         os.remove(dir + '/index.html')
-        with open(dir + '/data.json', 'w') as f:
-            json.dump(data, f)
+        os.remove(dir + '/metadata.opf')
         print("Extracted chapters:", *chapters, sep="\n")
     sys.exit(0)
--- moveBooks.py
+++ moveBooks.py
@@ -5,47 +5,54 @@ Script for moving the files created by divideBook into a next.js project directo
 import shutil
 import sys
 import os
+import json
 
 
-def move_book(book, dest):
+def move_book(book, dest, id):
+    print("book:", book)
+    with open(os.path.join(os.getcwd(), book, "data.json"), 'w+') as f:
+        print("f:", f)
+        data = json.load(f)
+        data["id"] = id
+        json.dump(data, f)
+
     htmldocs = [p for p in os.listdir(book) if os.path.splitext(p)[
         1] == '.html']
-
     if not os.path.exists(os.path.join(dest, "_book", book)):
         os.mkdir(os.path.join(dest, "_book", book))
-
     for doc in htmldocs:
         shutil.move(os.path.join(os.getcwd(), book, doc),
                     os.path.join(dest, "_book", book, doc))
-        print("moved " + os.path.join(os.getcwd(), book, doc) +
-              " to " + os.path.join(dest, "_book", book, doc))
 
     images = [i for i in os.listdir(os.path.join(book, "images"))]
-
     if not os.path.exists(os.path.join(dest, "public/assets/book", book)):
         os.mkdir(os.path.join(dest, "public/assets/book", book))
-
     for image in images:
         shutil.move(os.path.join(os.getcwd(), book, "images", image),
                     os.path.join(dest, "public/assets/book", book))
-        print("moved " + os.path.join(os.getcwd(), book, "images", image) +
-              " to " + os.path.join(dest, "public/assets/book", book))
 
     shutil.move(os.path.join(os.getcwd(), book, "style.css"),
-                os.path.join(dest, "styles/book", book + ".css"))
-    print("moved " + os.path.join(os.getcwd(), book, "style.css") +
-          " to " + os.path.join(dest, "styles/book", book + ".css"))
+                os.path.join(dest, "styles/book", id + ".css"))
 
     shutil.move(os.path.join(os.getcwd(), book, "data.json"),
                 os.path.join(dest, "data/book", book + ".json"))
-    print("moved " + os.path.join(os.getcwd(), book, "data.json") +
-          " to " + os.path.join(dest, "data/book", book + ".json"))
 
 
 if __name__ == '__main__':
     dest = sys.argv[1]
     books = sys.argv[2:]
     for book in books:
-        move_book(book, dest)
+      if (os.path.split(book)[0] and os.path.split(book)[1]):
+        book = os.path.split(book)[1]
+    print("books:", books)
+    maxId = 0
+    for j in os.listdir(os.path.join(dest, "data/book")):
+      with open(os.path.join(dest, "data/book", j), 'r') as f:
+        data = json.load(f)
+        if(data["id"] > maxId):
+          maxId = data["id"]
+    for book in books:
+        maxId += 1
+        move_book(book, dest, maxId)
 
     sys.exit(0)
